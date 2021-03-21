aa17a4e8ede1951260cf5742fa20aa563e47d192
Pavel Martynov <martynov.pl@gmail.com>
2021-03-20 22:49:15 -0700

    Added snapshots for compiler results

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,75 @@
 import { html } from "./index";
 
 describe("compiler", () => {
-  it("works", () => {
-    const result = html`<div></div>`;
-    expect(1).toBe(1);
+  describe("basic", () => {
+    it("basic 1", () => {
+      expect(html`<div></div>`.p).toMatchSnapshot();
+    });
+
+    it("basic 2", () => {
+      expect(
+        html`
+          <tr class=${"danger"}>
+            <td class="col-md-1">${1}</td>
+            <td class="col-md-4">
+              <a onclick=${() => {}}> ${"test"} </a>
+            </td>
+            <td class="col-md-1">
+              <a onclick=${() => {}}>
+                <span
+                  class="glyphicon glyphicon-remove"
+                  aria-hidden="true"
+                ></span>
+              </a>
+            </td>
+            <td class="col-md-6"></td>
+          </tr>
+        `.p,
+      ).toMatchSnapshot();
+    });
+  });
+
+  describe("afterNode", () => {
+    it("afterNode 1", () => {
+      expect(
+        html`
+          <div>
+            ${1}
+            <div></div>
+          </div>
+        `.p,
+      ).toMatchSnapshot();
+    });
+
+    it("afterNode 2", () => {
+      expect(
+        html`
+          <div>
+            ${1} ${2}
+            <div></div>
+          </div>
+        `.p,
+      ).toMatchSnapshot();
+    });
+  });
+
+  describe("fragments", () => {
+    it("fragments 1", () => {
+      expect(
+        html`
+          <div></div>
+          <div></div>
+        `.p,
+      ).toMatchSnapshot();
+    });
+
+    it("fragments 2", () => {
+      expect(
+        html`
+          <div>${1}</div>
+          <div>${2}</div>
+        `.p,
+      ).toMatchSnapshot();
+    });
   });
 });
